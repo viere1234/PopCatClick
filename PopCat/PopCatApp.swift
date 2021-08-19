6ac48931ae7593b4559d8cfb0fd2b044b27682ba
viere1234 <69096696+viere1234@users.noreply.github.com>
2021-08-19 21:32:31 +0800

    Initial Commit

---
+++ PopCat/PopCatApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopCatApp.swift
+//  PopCat
+//
+//  Created by 張智堯 on 2021/8/19.
+//
+
+import SwiftUI
+
+@main
+struct PopCatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
